d85455e7cc486318a68fc3e6e0fced25daf10d7d
jmgallego <jmgallego@106-09.info2>
2022-10-04 10:35:10 +0200

    First commit

--- EjerciciosTema1/src/ejerciciostema1/Exercise2.java
+++ EjerciciosTema1/src/ejerciciostema1/Exercise2.java
@@ -11,20 +11,20 @@ public class Exercise2 {
 		int need; //Variable declared and used to be printed at the end
 		int auxNeed; //Variable declared and used to be the number needed to add it to the asked one to be multiplier of 7
 
-		Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in); //Scanner is started to introduce data into variables
 
-		System.out.print("Introduzca un número para comprobar si es múltiplo de 7: ");
-		num = sc.nextInt();
-		System.out.println();
+		System.out.print("Introduzca un número para comprobar si es múltiplo de 7: "); //Printed line to inform the user which data he/she has to introduce and where
+		num = sc.nextInt(); //Scanner asks for a number to be checked, so it opens a space to let the user introduce the number
+		System.out.println(); //Blank line for a more visual interface
 
-		auxNeed = 7 - (num % 7);
+		auxNeed = 7 - (num % 7); //The rest is subtracted from 7 to know the distance to be multiplier of 7
 
-		need = auxNeed == -7 ? 7 : auxNeed;
-		need = (num % 7) == 0 ? 0 : auxNeed;
+		need = auxNeed == -7 ? 7 : auxNeed; //In case the user introduce "0", the distance will be -7, and to prevent this, if the program detect it it will show it positively.
+		need = (num % 7) == 0 ? 0 : auxNeed; //In case the user introduce a multiplier of 7, the program will show a distance of 0 instead of 7.
 
-		System.out.println("A " + num + " hace falta sumarle " + need + " para ser múltiplo de 7");
+		System.out.println("A " + num + " hace falta sumarle " + need + " para ser múltiplo de 7"); //The result will be showed together with the number that the user has introduced.
 
-		sc.close();
+		sc.close(); //Scanner is closed
 
 	}
 
